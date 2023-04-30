af8e92c8558c709a0ceae7e26e6186d73b0c78bf
Alex Watson <aswatson95@gmail.com>
2023-04-30 11:35:01 -0600

    logic changes

--- Board.hs
+++ Board.hs
@@ -1,63 +1,61 @@
 import Data.List (transpose)
-type Board = [[Char]]
-type Player = Char
 
-mkBoard :: Int -> Board
+mkBoard :: Int -> [[Char]]
 mkBoard n = replicate n (replicate n '.')
 
-mkPlayer :: Player
-mkPlayer = 'X'
+mkPlayer :: Int
+mkPlayer = 1
 
-mkOpponent :: Player
-mkOpponent = 'O'
+mkOpponent :: Int
+mkOpponent = 2
 
-size :: Board -> Int
-size bd = length bd
+size :: [[Char]] -> Int
+size = length
 
-row :: Int -> Board -> [Char]
+row :: Int -> [[Char]] -> [Char]
 row y bd = bd !! (y-1)
 
-column :: Int -> Board -> [Char]
-column x bd = map (!! (x-1)) bd
+column :: Int -> [[Char]] -> [Char]
+column x = map (!! (x-1))
 
-mark :: Int -> Int -> Board -> Player -> Board
+mark :: Int -> Int -> [[Char]] -> Char -> [[Char]]
 mark x y bd p = take (y-1) bd ++ [take (x-1) (bd !! (y-1)) ++ [p] ++ drop x (bd !! (y-1))] ++ drop y bd
 
-isEmpty :: Int -> Int -> Board -> Bool
+isEmpty :: Int -> Int -> [[Char]] -> Bool
 isEmpty x y bd = (bd !! (y-1)) !! (x-1) == '.'
 
-isMarked :: Int -> Int -> Board -> Bool
+isMarked :: Int -> Int -> [[Char]] -> Bool
 isMarked x y bd = not (isEmpty x y bd)
 
-isMarkedBy :: Int -> Int -> Board -> Player -> Bool
+isMarkedBy :: Int -> Int -> [[Char]] -> Char -> Bool
 isMarkedBy x y bd p = (bd !! (y-1)) !! (x-1) == p
 
-marker :: Int -> Int -> Board -> Player
+marker :: Int -> Int -> [[Char]] -> Char
 marker x y bd = (bd !! (y-1)) !! (x-1)
 
-isFull :: Board -> Bool
-isFull bd = all (\row -> all (\c -> c /= '.') row) bd
+isFull :: [[Char]] -> Bool
+isFull = all (notElem '.')
 
 diag :: [[a]] -> [a]
 diag m = [m !! i !! i | i <- [0..length m - 1]]
 
-isWonBy :: Board -> Char -> Bool
-isWonBy bd p = or (map (all (== p)) (rows ++ cols ++ diags))
+isWonBy :: [[Char]] -> Char -> Bool
+isWonBy bd p = any (all (== p)) (rows ++ cols ++ diags)
   where
     rows = bd
     cols = transpose bd
     diags = [diag bd, diag (map reverse bd)]
 
-isDraw :: Board -> Bool
+isDraw :: [[Char]] -> Bool
 isDraw bd = isFull bd && not (isWonBy bd 'X') && not (isWonBy bd 'O')
 
-isGameOver :: Board -> Bool
+isGameOver :: [[Char]] -> Bool
 isGameOver bd = isDraw bd || isWonBy bd 'X' || isWonBy bd 'O'
 
 boardToStr :: (Int -> Int -> Char) -> [[Int]] -> String
 boardToStr playerToChar bd =
   let header = "  x 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5\n"
       line = "y ------------------------------\n"
-      rowToString row = '|' : [playerToChar x y | x <- row, let y = head row, y /= -1] ++ "\n"
+      rowToString row = '|' : [playerToChar x y | let y = head row, y /= - 1, x <- row] ++ "\n"
       boardString = concat [rowToString row | row <- bd]
   in header ++ line ++ boardString
