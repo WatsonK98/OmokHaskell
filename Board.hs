aedd6ed27e348633b58dd9eeb9b57b5da684394d
Alex Watson <aswatson95@gmail.com>
2023-04-30 11:18:21 -0600

    fixed

--- Board.hs
+++ Board.hs
@@ -1,78 +1,63 @@
-type Board -> [[Int]]
+import Data.List (transpose)
+type Board = [[Char]]
+type Player = Char
 
-mkBoard :: Int -> [[Char]]
+mkBoard :: Int -> Board
 mkBoard n = replicate n (replicate n '.')
 
-mkPlayer :: Int
-mkPlayer = 1
+mkPlayer :: Player
+mkPlayer = 'X'
 
-mkOpponent :: Int
-mkOpponent = 2
+mkOpponent :: Player
+mkOpponent = 'O'
 
-size :: [[a]] -> Int
+size :: Board -> Int
 size bd = length bd
 
-row :: Int -> [[a]] -> [a]
+row :: Int -> Board -> [Char]
 row y bd = bd !! (y-1)
 
-column :: Int -> [[a]] -> [a]
+column :: Int -> Board -> [Char]
 column x bd = map (!! (x-1)) bd
 
-mark :: Int -> Int -> [[Int]] -> Int -> [[Int]]
-mark x y bd p = take (y-1) bd ++ [take (x-1) row ++ [p] ++ drop x row] ++ drop y bd
-  where row = bd !! (y-1)
+mark :: Int -> Int -> Board -> Player -> Board
+mark x y bd p = take (y-1) bd ++ [take (x-1) (bd !! (y-1)) ++ [p] ++ drop x (bd !! (y-1))] ++ drop y bd
 
-isEmpty :: Int-> Int -> [[Char]] -> Bool
-isEmpty x y bd = bd !! (y-1) !! (x-1) == '.'
+isEmpty :: Int -> Int -> Board -> Bool
+isEmpty x y bd = (bd !! (y-1)) !! (x-1) == '.'
 
-isMarked :: Int -> Int -> [[Char]] -> Bool
-isMarked x y bd = bd !! (y-1) !! (x-1) /= '.'
+isMarked :: Int -> Int -> Board -> Bool
+isMarked x y bd = not (isEmpty x y bd)
 
-ismarkedBy :: Int -> Int -> [[Int]] -> Int -> Bool
-ismarkedBy x y bd p = bd !! (y-1) !! (x-1) == p
+isMarkedBy :: Int -> Int -> Board -> Player -> Bool
+isMarkedBy x y bd p = (bd !! (y-1)) !! (x-1) == p
 
-marker :: Int -> Int -> [[Int]] -> Maybe Int
-marker x y bd
- | ismarkedBy x y bd 1 = Just 1
- | ismarkedBy x y bd 2 = Just 2
- | otherwise = Nothing
+marker :: Int -> Int -> Board -> Player
+marker x y bd = (bd !! (y-1)) !! (x-1)
 
-isFull :: [[Char]] -> Bool
-isFull bd = all (not . elem '.') bd
+isFull :: Board -> Bool
+isFull bd = all (\row -> all (\c -> c /= '.') row) bd
 
-countInARow :: Int -> [Int] -> Int
-countInARow p xs = maximum $ 0 : [n | ns <- group xs, head ns == p, let n = length ns]
+diag :: [[a]] -> [a]
+diag m = [m !! i !! i | i <- [0..length m - 1]]
 
-winningRow :: Int -> [Int] -> Bool
-winningRow p row = countInARow p row >= 5
-
-hasWinningRow :: [[Int]] -> Int -> Bool
-hasWinningRow bd p = any (winningRow p) bd
- where
-    winningRow p row = countInARow p row >= 5
-    countInARow p [] = 0
-    countInARow p (x:xs)
-     | x == p = 1 + countInARow p xs
-     | otherwise = 0 + countInARow p xs
-
-isWonBy :: [[Int]] -> Int -> Bool
-isWonBy bd p = any (hasWinningRow p) (rows bd ++ cols bd ++ diags bd)
- where
-    rows = id
-    cols = transpose
-    diags bd = [diag, diag . map reverse]
-     where diag = [bd !! i !! i | i <= [0..length bd - 1]]
+isWonBy :: Board -> Char -> Bool
+isWonBy bd p = or (map (all (== p)) (rows ++ cols ++ diags))
+  where
+    rows = bd
+    cols = transpose bd
+    diags = [diag bd, diag (map reverse bd)]
 
 isDraw :: Board -> Bool
-isDraw bd = all (not . isEmptyPlace bd) [(x,y) | x <- [1..size bd], y <- [1..size bd]]
+isDraw bd = isFull bd && not (isWonBy bd 'X') && not (isWonBy bd 'O')
 
 isGameOver :: Board -> Bool
-isGameOver bd = isFull bd || isWonBy bd 1 || isWonBy bd 2
+isGameOver bd = isDraw bd || isWonBy bd 'X' || isWonBy bd 'O'
 
-boardToStr :: (Player -> Char) -> Board -> String
-boardToStr playertoChar bd = unlines $ header : seperator : body
- where
-    header = " x 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5"
-    separator = " y ---------------------------"
-    body = [ rowToStr y | y <- [boardHeigh, boardHeigh -1 .. 1] ]
-    rowToStr y = intToDigit y : "| " ++ [ playerToChar (bd get (x, y)) | x <- [1..boardWidth]] ++ " |"
+boardToStr :: (Int -> Int -> Char) -> [[Int]] -> String
+boardToStr playerToChar bd =
+  let header = "  x 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5\n"
+      line = "y ------------------------------\n"
+      rowToString row = '|' : [playerToChar x y | x <- row, let y = head row, y /= -1] ++ "\n"
+      boardString = concat [rowToString row | row <- bd]
+  in header ++ line ++ boardString
